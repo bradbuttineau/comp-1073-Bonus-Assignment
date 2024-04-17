07b4ea5faf810469d0afa4807354c57e5a0a1ec3
Brad Buttineau <113149072+BradB87@users.noreply.github.com>
2024-04-16 21:08:48 -0400

    commit 4

--- reflect.js
+++ reflect.js
@@ -9,39 +9,46 @@ const first = Reflect.get(person1, 'first');
 const last = Reflect.get(person1, 'last');
 const occupation = Reflect.get(person1, 'occupation');
 
-console.log(first, last, occupation);
-
-
+console.log(Reflect.get(person1, 'name', 'last', age, 'occupation'));
 
 //Reflect Set //
-const person2 = {
-    first: 'Scott',
-    last: 'McCrindle',
-    occupation: 'Javascript Instructor',
+const person2 = {};
+Reflect.set(person2, 'name', 'Scott', 'last', 'McCrindle', 'occupation', 'Javascript Instructor');
 
+console.log(person2.name, person2.last, person2.occupation);
+
+//Reflect DefineProperty()//
+const object1 = {};
+
+if (Reflect.defineProperty(object1, 'property1', { value: 777 })) {
+    console.log('property1 created!');
+    // Expected output: "property1 created!"
+} else {
+    console.log('problem creating property1');
 }
-Reflect.set(person2, first, 'Scott', last, 'McCrindle', occupation, 'Javascript Instructor');
 
-console.log(person2.first, person2.last, person2.occupation);
+console.log(object1.property1);
+// Expected output: 777
 
-//Reflect Apply//
-const person3 = {
-    first: 'Brad',
-    last: 'Buttineau',
-    age: 36,
-    occupation: 'Student',
 
-    introSelf() {
-        console.log(`Hello ${this.first}, ${this.last}, ${this.age}, ${this.occupation}`);
-    }
+// Reflect DeleteProperty()//
+const object2 = {
+    property1: 42,
 };
-// Reflect Construct//
-function Person4(name, occupation, quote) {
-    this.name = name;
-    this.occupation = occupation;
-    this.quote = quote;
-}
 
-const personName = Reflect.construct(Person4, ['Buzz Lightyear', 'Space Ranger', ' I protect the galaxy from the threat of invasion. I come in peace']);
+Reflect.deleteProperty(object1, 'two');
 
-console.log(personName.name, personName.occupation, personName.quote);
+console.log(object1.property1);
+// Expected output: undefined//
+
+const array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+Reflect.deleteProperty(array2, '7');
+
+console.log(array2);
+// Expected output: Array [1, 2, 3, 4, 5, 6, 8, 9, 10]//
+
+
+
+
+
+
